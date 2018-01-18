cbe25faf9544788d9ba55c180fd9bce850a70223
befort <[1053094373@qq.com]>
2018-01-18 21:59:48 +0800

    test

--- src/myAnAction.java
+++ src/myAnAction.java
@@ -1,5 +1,8 @@
 import com.sun.org.apache.xpath.internal.SourceTree;
 
+import javax.swing.event.AncestorEvent;
+
 public class myAnAction {
 	SourceTree tree = new SourceTree(hashCode(), toString());
+	long ancestorEvent = AncestorEvent.ACTION_EVENT_MASK;
 }
